dcfbb9f69a32d989d8496fd8ee2277a6575fbfe1
chelseo <chelseoleonel1612@gmail.com>
2025-05-12 22:30:52 +0100

    wrapping the useSearchParams in a suspense bouandary

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
@@ -8,10 +8,7 @@ import Form from "@components/Form";
 const EditPrompt = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  console.log("All search params:", Array.from(searchParams.entries()));
-  console.log("Seb: " + searchParams);
-  const promptId = searchParams.get('id');
-  console.log("Leonel: " + promptId);
+  const promptId = searchParams.get("id");
 
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
@@ -21,17 +18,17 @@ const EditPrompt = () => {
 
   useEffect(() => {
     const getPromptDetails = async () => {
-        const response = await fetch(`/api/prompt/${promptId}`)
-        const data = await response.json();
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
 
-        setPost({
-            prompt: data.prompt,
-            tag: data.tag,
-        })
-    }
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
 
-    if(promptId) {
-        getPromptDetails();
+    if (promptId) {
+      getPromptDetails();
     }
   }, [promptId]);
 
@@ -39,7 +36,7 @@ const EditPrompt = () => {
     e.preventDefault();
     setSubmitting(true);
 
-    if(!promptId) return alert("Prompt ID not found.")
+    if (!promptId) return alert("Prompt ID not found.");
 
     try {
       const response = await fetch(`api/prompt/${promptId}`, {
@@ -61,13 +58,15 @@ const EditPrompt = () => {
   };
 
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense>
+      <Form
+        type="Edit"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    </Suspense>
   );
 };
 
